ffb8251222ed26472dbd7998a02e5b3c1648515b
Pankaj Suthar <pankajsuthar27302@gmail.com>
2024-04-09 22:13:05 +0530

    updated BlueJay Delivery website

--- mail.php
+++ mail.php
@@ -1,17 +1,16 @@
+
+
 <?php
+$to = "psuthar27302@gmail.com"; // Your email address
+$from = $_POST['email']; // Sender's email address
+$sender_name = $_POST['name'];
+$phone = $_POST['phone'];
+$address = $_POST['address'];
+$note = $_POST['note'];
 
-	$to = "md.shahinfb24@gmail.com"; // this is your Email address
-	$from  = $_POST['email']; // this is the sender's Email address
-	$sender_name = $_POST['name'];
-	$phone = $_POST['phone'];
-	$address = $_POST['address'];
-	$note = $_POST['note'];
+$subject = "Form submission";
+$message = $sender_name . " has sent the contact message! Their phone number is: " . $phone . ", and their address is: " . $address . ". They wrote the following: " . "\n\n" . $note;
 
-
-	$subject = "Form submission";
-	$message = $sender_name . " has send the contact message! His/ her phone number is : " .  $phone . ", and his / her address is " . $address . ". He / she wrote the following... ". "\n\n" . $note;
-
-	$headers = 'From: ' . $from;
-	mail($to, $subject, $message, $headers);
-
+$headers = 'From: ' . $from;
+mail($to, $subject, $message, $headers);
 ?>
